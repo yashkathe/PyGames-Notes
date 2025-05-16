ade83909b77c2c3b619f0dec10325f59780f08c4
Yash Kathe <yashkathe123@gmail.com>
2025-05-16 12:50:12 -0700

    simple animation

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ clock = pygame.time.Clock()
 test_font = pygame.font.FontType("font/Pixeltype.ttf", 50)  # font_type, font_size
 
 # surface
-sky_surface = pygame.image.load("graphics/sky.png")
-ground_surface = pygame.image.load("graphics/ground.png")
+sky_surface = pygame.image.load("graphics/sky.png").convert()
+ground_surface = pygame.image.load("graphics/ground.png").convert()
 text_surface = test_font.render("My Game", True, "Black")  # text, antialias?, color
+
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_x_pos, snail_y_pos = 300, 280
 
 # main game logic
 while True:
@@ -26,6 +29,13 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (325, 100))
+
+    snail_x_pos -= 5
+
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+
+    screen.blit(snail_surface, (snail_x_pos, snail_y_pos))
 
     pygame.display.update()
     clock.tick(60)
