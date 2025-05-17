0f7a03afa252a29f15651fa6d6560a8845da5ebc
Yash Kathe <yashkathe123@gmail.com>
2025-05-17 13:22:47 -0700

    using rectangles for positioning

--- main.py
+++ main.py
@@ -16,7 +16,11 @@ ground_surface = pygame.image.load("graphics/ground.png").convert()
 text_surface = test_font.render("My Game", True, "Black")  # text, antialias?, color
 
 snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-snail_x_pos, snail_y_pos = 300, 280
+snail_rect = snail_surface.get_rect(midbottom=(600, 300))
+
+player_surface = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
+# rectangle
+player_rect = player_surface.get_rect(midbottom=(80, 300))
 
 # main game logic
 while True:
@@ -30,12 +34,13 @@ while True:
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (325, 100))
 
-    snail_x_pos -= 5
+    snail_rect.x -= 4
 
-    if snail_x_pos < -100:
-        snail_x_pos = 800
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
 
-    screen.blit(snail_surface, (snail_x_pos, snail_y_pos))
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect)
 
     pygame.display.update()
     clock.tick(60)
